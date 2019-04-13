196f4d3e1d972f52c658a27927d9b345af12d049
David Bohorquez <davidbcaro@gmail.com>
2019-04-13 07:40:08 -0500

    chore: Se agregan pruebas

---
+++ ADSI_1349397_trimestre_5/EON/04-Prueba/practica/practica/Models/TrainingLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace practica.Models
+{
+    public class TrainingLevel
+    {
+        [Key]
+        [Required(ErrorMessage = "*El campo {0} es obligatorio*")]
+        public string trainingLevel { get; set; }
+        [Required(ErrorMessage = "*El campo {0} es obligatorio*")]
+        public int State { get; set; }
+    }
+}
---
+++ ADSI_1349397_trimestre_5/EON/04-Prueba/practica/practica/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace practica.Models
+{
+    public class User
+    {
+
+
+        [Required (ErrorMessage = "campo obligatorio")  ]
+        public string IdDocument { get; set; }
+        [Key]
+        [Required(ErrorMessage = "campo obligatorio")]
+        public int Docuemt { get; set; }
+        [Required(ErrorMessage = "campo obligatorio")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "campo obligatorio")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "campo obligatorio")]
+        public string Correo { get; set; }
+     
+    }
+}
